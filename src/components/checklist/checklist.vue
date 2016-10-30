0c2b702453900b5d9b5b80f3fa908469a17d2b04
tianyong90 <412039588@qq.com>
2016-10-30 18:29:34 +0800

    checklist初步

--- src/components/checklist/checklist.vue
+++ src/components/checklist/checklist.vue
@@ -1,15 +1,61 @@
 <template>
+  <div>
+    <div class="weui-cells__title">复选列表项</div>
+    <div class="weui-cells weui-cells_checkbox">
+        <label v-for="option in options" class="weui-cell weui-check__label" for="s11">
+            <div class="weui-cell__hd">
+                <input type="checkbox" class="weui-check" v-model="currentValue" :disabled="option.disabled" :value="option.value || option" id="s11">
+                <i class="weui-icon-checked"></i>
+            </div>
+            <div class="weui-cell__bd">
+                <p>{{ option.label || option }}</p>
+            </div>
+        </label>
+        <a href="javascript:void(0);" class="weui-cell weui-cell_link">
+            <div class="weui-cell__bd">添加更多</div>
+        </a>
+    </div>
+  </div>
 </template>
 
 <script type="text/babel">
 import 'weui/dist/style/weui.min.css'
 
 export default {
-  name: 'vui-radio',
+  name: 'vui-checklist',
 
   props: {
-    disabled: Boolean,
-    value: Boolean
+    max: Number,
+    title: String,
+    align: String,
+    options: {
+      type: Array,
+      required: true
+    },
+    value: Array
+  },
+
+  data () {
+    return {
+      currentValue: this.value
+    }
+  },
+
+  computed: {
+    limit () {
+      return this.max < this.currentValue.length
+    }
+  },
+
+  watch: {
+    value (val) {
+      this.currentValue = val
+    },
+
+    currentValue (val) {
+      if (this.limit) val.pop()
+      this.$emit('input', val)
+    }
   }
 }
 </script>
--- src/demos/checklist.vue
+++ src/demos/checklist.vue
@@ -1,11 +1,29 @@
 <template>
 	<div>
-
+		<vui-checklist :options="options" v-model="checkedItems"></vui-checklist>
 	</div>
 </template>
 
 <script type="text/babel">
 export default {
+  data () {
+    return {
+      options: [
+        {
+          label: '选项1',
+          value: 'value1'
+        },
+        {
+          label: '选项2',
+          value: 'value2'
+        },
+        {
+          label: '选项3',
+          value: 'value3'
+        }
+      ]
+    }
+  },
   mounted () {
     console.log(this.$route)
   }
