032f38d515078d646e30c558134419132fdb409a
tianyong90 <412039588@qq.com>
2016-11-03 11:01:31 +0800

    加入icons

---
+++ src/components/icons/icons.vue
@@ -0,0 +1,40 @@
+<template>
+  
+</template>
+
+<script type="text/babel">
+import 'weui/dist/style/weui.min.css'
+
+export default {
+  name: 'vui-icon',
+
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    disabled: Boolean,
+    mini: Boolean,
+    plain: Boolean
+  },
+
+  methods: {
+    handleClick ($event) {
+      $event.preventDefault()
+    }
+  },
+
+  computed: {
+    classObject: function () {
+      let classType = this.plain ? `weui-btn_plain-${this.type}` : `weui-btn_${this.type}`
+      let classDisabled = this.plain ? 'weui-btn_plain-disabled' : 'weui-btn_disabled'
+
+      return {
+        [classType]: true,
+        [classDisabled]: this.disabled,
+        'weui-btn_mini': this.mini
+      }
+    }
+  }
+}
+</script>
---
+++ src/demos/icons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script type="text/babel">
+export default {
+  mounted () {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
