be41458a99caf20178c16f76a2719ee522f0a830
tianyong90 <412039588@qq.com>
2016-12-25 00:45:07 +0800

    配置调整

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,7 @@ var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/index.js'
   },
   output: {
     path: config.build.assetsRoot,
