4b90c63726a523775b0bf8c8da9335016701a897
tianyong90 <412039588@qq.com>
2017-01-03 19:35:39 +0800

    dialog完善中

--- example/pages/dialog.vue
+++ example/pages/dialog.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="page page-with-padding buttons">
-    <wv-button type="primary" @click.native="showDialog()">primary</wv-button>
-    <wv-button type="primary" @click.native="showDialog()">primary</wv-button>
+    <wv-button type="default" @click.native="showDialog('ios', 'style1')">iOS Dialog 样式一</wv-button>
+    <wv-button type="default" @click.native="showDialog('ios', 'style2')">iOS Dialog 样式二</wv-button>
+    <wv-button type="default" @click.native="showDialog('android', 'style1')">Android Dialog 样式一</wv-button>
+    <wv-button type="default" @click.native="showDialog('android', 'style2')">Android Dialog 样式二</wv-button>
   </div>
 </template>
 
 <script type="text/babel">
+import Dialog from '../../src/components/dialog'
+
 export default {
   methods: {
-    showDialog () {
-      console.log(123)
+    showDialog (type, style) {
+      Dialog('haha')
     }
   }
 }
