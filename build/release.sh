70f39cb2ecb49f560cea1331dc093b4058dd7593
tianyong90 <412039588@qq.com>
2017-01-04 12:34:09 +0800

    加入release.sh

---
+++ build/release.sh
@@ -0,0 +1,23 @@
+set -e
+echo "[we-vue]Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  VERSION=$VERSION npm run build:all
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push origin master
+  npm publish
+fi
