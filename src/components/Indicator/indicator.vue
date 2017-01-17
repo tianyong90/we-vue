cff05172a9c7a0497d2a82046610ed5150e0d549
tianyong90 <412039588@qq.com>
2017-01-17 13:44:44 +0800

    删除模板中不必要的ID

--- src/components/Indicator/indicator.vue
+++ src/components/Indicator/indicator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="loadingToast" v-show="visible">
+  <div v-show="visible">
     <div class="weui-mask_transparent"></div>
     <div class="weui-toast">
       <i class="weui-loading weui-icon_toast"></i>
