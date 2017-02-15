babccf3bc224bee9f0cbb54100f1e2406c5e96b9
tianyong90 <412039588@qq.com>
2017-02-15 18:43:18 +0800

    backup

---
+++ build/build-all.js
@@ -0,0 +1,25 @@
+// https://github.com/shelljs/shelljs
+require('./check-versions')()
+require('shelljs/global')
+env.NODE_ENV = 'production'
+
+var path = require('path')
+var config = require('../config')
+var ora = require('ora')
+var webpack = require('webpack')
+var webpackConfig = require('./webpack.common.conf')
+
+var spinner = ora('building commonjs...')
+spinner.start()
+
+webpack(webpackConfig, function (err, stats) {
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})
