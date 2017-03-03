defb27760e47d3220c3818a643f3537b35d2f434
tianyong90 <412039588@qq.com>
2017-03-03 16:42:25 +0800

    button支持loading状态

--- example/pages/button.vue
+++ example/pages/button.vue
@@ -2,18 +2,21 @@
 	<div class="page-with-padding">
 		<div class="buttons-big">
 			<wv-button type="primary">页面主要操作 Normal</wv-button>
-			<wv-button type="primary" :disabled="true">页面主要操作 Disabled</wv-button>
+			<wv-button type="primary" is-loading>页面主要操作 Loading</wv-button>
+			<wv-button type="primary" disabled>页面主要操作 Disabled</wv-button>
 			<wv-button type="default">页面次要操作 Normal</wv-button>
-			<wv-button type="default" :disabled="true">页面次要操作 Disabled</wv-button>
+			<wv-button type="default" is-loading>页面次要操作 Loading</wv-button>
+			<wv-button type="default" disabled>页面次要操作 Disabled</wv-button>
 			<wv-button type="warn">警告类操作 Normal</wv-button>
-			<wv-button type="warn" :disabled="true">警告类操作 Disabled</wv-button>
+			<wv-button type="warn" is-loading>警告类操作 Loading</wv-button>
+			<wv-button type="warn" disabled>警告类操作 Disabled</wv-button>
 		</div>
 
 		<div class="buttons-plain">
 			<wv-button type="default" :plain="true">按钮</wv-button>
-			<wv-button type="default" :plain="true" :disabled="true">按钮</wv-button>
+			<wv-button type="default" :plain="true" disabled>按钮</wv-button>
 			<wv-button type="primary" :plain="true">按钮</wv-button>
-			<wv-button type="primary" :plain="true" :disabled="true">按钮</wv-button>
+			<wv-button type="primary" :plain="true" disabled>按钮</wv-button>
 		</div>
 
 		<div class="buttons-mini">
@@ -36,13 +39,13 @@ export default {
 		display: block;
 		margin: 10px auto;
 	}
-	
+
 	.buttons-plain {
 		display: block;
 		width: 60%;
 		margin: 10px auto;
 	}
-	
+
 	.buttons-mini {
 		display: block;
 		width: 60%;
