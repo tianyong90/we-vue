5cc6df9c20226361ac054032e53eee32791ab92f
tianyong90 <412039588@qq.com>
2017-08-12 13:57:55 +0800

    indicator 文件夹错误修正

---
+++ src/components/indicator/indicator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="weui-toast" v-show="visible">
+    <wv-spinner class="weui-icon_toast" v-if="spinnerType !=='none'" :type="spinnerType" :size="25"></wv-spinner>
+    <p class="weui-toast__content" v-text="text"></p>
+  </div>
+</template>
+
+<script>
+  import WvSpinner from '../spinner/index'
+
+  export default {
+    components: {
+      WvSpinner
+    },
+
+    props: {
+      text: String,
+      spinnerType: {
+        type: String
+      }
+    },
+
+    data () {
+      return {
+        visible: false
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
