68585fe0c3d260a0b41fd13787b52efd34caa4c2
= <=>
2017-09-24 10:41:30 +0800

    修复小问题

--- example/pages/swipe.vue
+++ example/pages/swipe.vue
@@ -22,14 +22,32 @@
       <wv-swipe-item class="demo-swipe-item" style="background-color: #ffc107">2</wv-swipe-item>
       <wv-swipe-item class="demo-swipe-item" style="background-color: #03a9f4">3</wv-swipe-item>
     </wv-swipe2> -->
-
-    <wv-swipe3 class="demo-swipe" :height="130" :auto="0" :continuous="false">
+    <div class="description">
+      循环
+    </div>
+    <wv-swipe3 class="demo-swipe" :height="130" :auto="0" :continuous="true">
       <wv-swipe-item class="demo-swipe-item" style="background-color: #f44336"><div class="page1">1</div></wv-swipe-item>
       <wv-swipe-item class="demo-swipe-item" style="background-color: #ffc107">2</wv-swipe-item>
       <wv-swipe-item class="demo-swipe-item" style="background-color: #03a9f4">3</wv-swipe-item>
     </wv-swipe3>
 
+    <div class="description">
+      循环|间隔10px
+    </div>
+    <wv-swipe3 class="demo-swipe" :height="130" :auto="0" :continuous="true" :gap="10">
+      <wv-swipe-item class="demo-swipe-item" style="background-color: #f44336"><div class="page1">1</div></wv-swipe-item>
+      <wv-swipe-item class="demo-swipe-item" style="background-color: #ffc107">2</wv-swipe-item>
+      <wv-swipe-item class="demo-swipe-item" style="background-color: #03a9f4">3</wv-swipe-item>
+    </wv-swipe3>
     
+    <div class="description">
+      不循环|溢出缓动
+    </div>
+    <wv-swipe3 class="demo-swipe" :height="130" :auto="0" :continuous="false" overflow="backDrag">
+      <wv-swipe-item class="demo-swipe-item" style="background-color: #f44336"><div class="page1">1</div></wv-swipe-item>
+      <wv-swipe-item class="demo-swipe-item" style="background-color: #ffc107">2</wv-swipe-item>
+      <wv-swipe-item class="demo-swipe-item" style="background-color: #03a9f4">3</wv-swipe-item>
+    </wv-swipe3>
   </div>
 </template>
 
@@ -49,5 +67,11 @@
         height: 200px;
       }
     }
+  }
+
+  .description {
+    padding-bottom: 20px;
+    font-size: 16px;
+    text-align: center;
   }
 </style>
