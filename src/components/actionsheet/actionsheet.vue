bb92f2cc2321a2017ab53e262ef0515a1ca60cf0
= <=>
2017-10-02 16:22:39 +0800

    修复actionsheet通过translate居中导致字体模糊

--- src/components/actionsheet/actionsheet.vue
+++ src/components/actionsheet/actionsheet.vue
@@ -87,4 +87,22 @@
     transform: scale(1, 1);
     background-color: rgba(0, 0, 0, 0.6);
   }
+
+  .weui-skin_android {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .weui-actionsheet {
+    display: inline-block;
+    flex: 0 0 auto;
+    position: static;
+    transform: unset;
+  }
 </style>
