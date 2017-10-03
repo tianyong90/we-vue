c6ef7ec3e61cd79ec627e82e94c62e8a7a94c3d7
= <=>
2017-10-03 19:26:10 +0800

    infinite-scroll示例优化loading动画时出现大量layer闪动的情况

---
+++ example/pages/android-list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  
+</style>
