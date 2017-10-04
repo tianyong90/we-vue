7cde7386cb7cd833ed123ae3d54e6ad1f97e46b7
= <=>
2017-10-04 16:23:19 +0800

    cell-swipe更新滑动动画,还有修复垂直滚动问题,其实感觉这是为了效果做效果, 行为本身都没有考虑其他行为, 不过transform的使用让我开眼界了

--- src/components/cell-swipe/cell-swipe.vue
+++ src/components/cell-swipe/cell-swipe.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="weui-cell weui-cell_swiped">
-    <div class="weui-cell__bd" ref="cellBd" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
+    <div class="weui-cell__bd" ref="cellBd" v-swipe:horizonal.lock="swipeConfig" @touchstart="touchStart">
       <wv-cell :title="title" :value="value" :is-link="isLink" :to="to">
         <template slot="icon">
           <slot name="icon"></slot>
@@ -22,12 +22,20 @@
 <script>
   import Cell from '../cell/index'
   import Transform from 'css3transform'
+  import { swipeDirective } from '../../custom/event/swipe.js'
 
   export default {
     name: 'wv-cell-swipe',
 
     components: {
       Cell
+    },
+
+    created (){
+      this.swipeConfig = {
+        onSwipe: this.touchMove,
+        onSwipeDone: this.touchEnd
+      }
     },
 
     props: {
@@ -50,16 +58,17 @@
     mounted () {
       this.isDragging = false
       const cellBd = this.$refs.cellBd
+      const rightBtns = this.$refs.rightBtns
       Transform(cellBd, true)
+      Transform(rightBtns, true)
     },
 
     methods: {
       touchStart (event) {
-        event.preventDefault()
-
         if (this.isDragging) return
 
         const cellBd = this.$refs.cellBd
+        const rightBtns = this.$refs.rightBtns
 
         if (event.type === 'touchstart') {
           this.dragState.startPositionX = event.changedTouches[0].clientX
@@ -71,21 +80,16 @@
         this.dragState.startTimestamp = new Date()
 
         cellBd.style.transition = ''
+        rightBtns.style.transition = ''
       },
 
-      touchMove (event) {
-        event.preventDefault()
+      touchMove (info) {
 
         this.isDragging = true
 
-        let deltaX
-        if (event.type === 'touchmove') {
-          deltaX = event.changedTouches[0].clientX - this.dragState.startPositionX
-        } else {
-          deltaX = event.clientX - this.dragState.startPositionX
-        }
-
+        let deltaX = info.movingX - this.dragState.startPositionX
         const cellBd = this.$refs.cellBd
+        const rightBtns = this.$refs.rightBtns
         const btnsWidth = this.$refs.rightBtns.clientWidth
 
         let targetTranslateX
@@ -95,21 +99,17 @@
           targetTranslateX = this.dragState.startTranslateX + deltaX < 0 ? this.dragState.startTranslateX + deltaX : 0
         }
         cellBd.translateX = targetTranslateX
+        rightBtns.translateX = (targetTranslateX + btnsWidth)
       },
 
-      touchEnd (event) {
-        event.preventDefault()
-
+      touchEnd (info) {
         this.isDragging = false
 
         const cellBd = this.$refs.cellBd
+        const rightBtns = this.$refs.rightBtns
         const btnsWidth = this.$refs.rightBtns.clientWidth
 
-        if (event.type === 'touchend') {
-          this.dragState.endPositionX = event.changedTouches[0].clientX
-        } else {
-          this.dragState.endPositionX = event.clientX
-        }
+        this.dragState.endPositionX = info.movingX
         this.dragState.endTranslateX = cellBd.translateX
         this.dragState.totalDeltaX = this.dragState.endPositionX - this.dragState.startPositionX
 
@@ -117,31 +117,43 @@
 
         const touchTime = this.dragState.endTimestamp - this.dragState.startTimestamp
 
-        // 500ms 内当作点击处理
-        if (touchTime <= 500 && parseInt(this.dragState.totalDeltaX) === 0) {
-          this.$children[0].$emit('CLICK_IN_CELLSWIPE', event)
-        }
-
         if (this.dragState.startTranslateX === 0 && this.dragState.totalDeltaX < 0) {
           if (Math.abs(this.dragState.totalDeltaX) >= 30) {
             cellBd.translateX = -btnsWidth
+            rightBtns.translateX = 0
           } else {
             cellBd.translateX = 0
+            rightBtns.translateX = btnsWidth
           }
           cellBd.style.transition = 'all 200ms ease'
+          rightBtns.style.transition = 'all 200ms ease'
         } else if (this.dragState.startTranslateX === -btnsWidth && this.dragState.totalDeltaX > 0) {
           if (Math.abs(this.dragState.totalDeltaX) >= 30) {
             cellBd.translateX = 0
+            rightBtns.translateX = btnsWidth
           } else {
             cellBd.translateX = -btnsWidth
+            rightBtns.translateX = 0
           }
           cellBd.style.transition = 'all 200ms ease'
+          rightBtns.style.transition = 'all 200ms ease'
         }
         this.dragState = {}
       }
+    },
+
+    directives: {
+      swipe: swipeDirective
     }
   }
 </script>
 
 <style scoped lang="scss">
+  .weui-cell_swiped{
+    padding: 0;
+
+    & > .weui-cell__ft{
+      transform: translateX(100%);
+    }
+  }
 </style>
