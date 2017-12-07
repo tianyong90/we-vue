0a9f5ce8ee35c5da2f8511cb6bc6971b39ea4fdb
tianyong90 <412039588@qq.com>
2017-12-07 22:29:47 +0800

    flex-item 单元测试

---
+++ test/unit/specs/FlexItem.spec.js
@@ -0,0 +1,62 @@
+import { shallow, mount } from 'vue-test-utils'
+import Flex from '@/components/flex'
+import FlexItem from '@/components/flex-item'
+
+describe('flex-item', () => {
+  let parentWrapper
+  let wrapper
+  afterEach(() => {
+    wrapper && wrapper.destroy()
+    parentWrapper && parentWrapper.destroy()
+  })
+
+  it('create', () => {
+    parentWrapper = mount(Flex, {
+      propsData: {
+        animate: true
+      }
+    })
+
+    let wrapper = shallow(FlexItem, {
+      parent: parentWrapper.vm
+    })
+
+    expect(wrapper.name()).toBe('wv-flex-item')
+    expect(wrapper.hasClass('weui-flex__item')).toBeTruthy()
+  })
+
+  it('comput gutter', () => {
+    parentWrapper = mount(Flex, {
+      propsData: {
+        gutter: 10
+      }
+    })
+
+    let wrapper = shallow(FlexItem, {
+      parent: parentWrapper.vm
+    })
+
+    expect(wrapper.vm.gutter).toBe(wrapper.vm.$parent.gutter)
+  })
+
+  it('comput style', () => {
+    parentWrapper = mount(Flex, {
+      propsData: {}
+    })
+
+    let wrapper = shallow(FlexItem, {
+      parent: parentWrapper.vm
+    })
+
+    let computedStyle = {
+      flex: wrapper.vm.flex
+    }
+
+    if (wrapper.vm.gutter) {
+      computedStyle.paddingLeft = wrapper.vm.gutter / 2 + 'px'
+      computedStyle.paddingRight = computedStyle.paddingLeft
+    }
+
+    expect(wrapper.vm.style).toEqual(computedStyle)
+  })
+})
