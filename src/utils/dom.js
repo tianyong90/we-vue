7720df21c42446a35ee3827c0220e2a9d600f479
tianyong90 <412039588@qq.com>
2017-12-07 11:24:44 +0800

    代码规范

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -5,18 +5,16 @@ const SPECIAL_CHARS_REGEXP = /([:\-_]+(.))/g
 const MOZ_HACK_REGEXP = /^moz([A-Z])/
 const ieVersion = isServer ? 0 : Number(document.documentMode)
 
-/* istanbul ignore next */
 const trim = function (string) {
   return (string || '').replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, '')
 }
-/* istanbul ignore next */
+
 const camelCase = function (name) {
   return name.replace(SPECIAL_CHARS_REGEXP, function (_, separator, letter, offset) {
     return offset ? letter.toUpperCase() : letter
   }).replace(MOZ_HACK_REGEXP, 'Moz$1')
 }
 
-/* istanbul ignore next */
 export const on = (function () {
   if (!isServer && document.addEventListener) {
     return function (element, event, handler) {
@@ -33,7 +31,6 @@ export const on = (function () {
   }
 })()
 
-/* istanbul ignore next */
 export const off = (function () {
   if (!isServer && document.removeEventListener) {
     return function (element, event, handler) {
@@ -50,7 +47,6 @@ export const off = (function () {
   }
 })()
 
-/* istanbul ignore next */
 export const once = function (el, event, fn) {
   var listener = function () {
     if (fn) {
@@ -61,7 +57,6 @@ export const once = function (el, event, fn) {
   on(el, event, listener)
 }
 
-/* istanbul ignore next */
 export function hasClass (el, cls) {
   if (!el || !cls) return false
   if (cls.indexOf(' ') !== -1) throw new Error('className should not contain space.')
@@ -70,9 +65,8 @@ export function hasClass (el, cls) {
   } else {
     return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1
   }
-};
+}
 
-/* istanbul ignore next */
 export function addClass (el, cls) {
   if (!el) return
   var curClass = el.className
@@ -93,9 +87,8 @@ export function addClass (el, cls) {
   if (!el.classList) {
     el.className = curClass
   }
-};
+}
 
-/* istanbul ignore next */
 export function removeClass (el, cls) {
   if (!el || !cls) return
   var classes = cls.split(' ')
@@ -116,9 +109,8 @@ export function removeClass (el, cls) {
   if (!el.classList) {
     el.className = trim(curClass)
   }
-};
+}
 
-/* istanbul ignore next */
 export const getStyle = ieVersion < 9 ? function (element, styleName) {
   if (isServer) return
   if (!element || !styleName) return null
@@ -155,7 +147,6 @@ export const getStyle = ieVersion < 9 ? function (element, styleName) {
   }
 }
 
-/* istanbul ignore next */
 export function setStyle (element, styleName, value) {
   if (!element || !styleName) return
 
@@ -173,4 +164,4 @@ export function setStyle (element, styleName, value) {
       element.style[styleName] = value
     }
   }
-};
+}
