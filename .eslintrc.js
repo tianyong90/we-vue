47f9aa5ff32cd023253de9a8d13a13f00c20cff2
tianyong90 <412039588@qq.com>
2017-12-26 20:20:24 +0800

    install eslint-plugin-vue, adjust .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,42 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
     sourceType: 'module'
   },
   env: {
-    browser: true,
+    es6: true,
+    node: true,
+    mocha: true
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: 'plugin:vue/strongly-recommended',
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/jsx-uses-vars': 2,
+    'vue/require-v-for-key': 0,
+    "vue/require-default-prop": 0,
+    "vue/name-property-casing": 0,
+    "vue/no-unused-vars": 0,
+    'vue/max-attributes-per-line': [2, {
+      "singleline": 5,
+      "multiline": {
+        "max": 5,
+        "allowFirstLine": false
+      }
+    }]
   }
 }
