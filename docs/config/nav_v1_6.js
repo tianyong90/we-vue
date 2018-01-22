e801fb2e64ab959e04453a6b84693cc2c246d55f
tianyong90 <412039588@qq.com>
2018-01-23 01:32:41 +0800

    [docs] improve sidebar nav config

---
+++ docs/config/nav_v1_6.js
@@ -0,0 +1,216 @@
+export default [
+  {
+    title: '开始使用',
+    groups: [
+      {
+        list: [
+          {
+            title: '概述',
+            path: '/doc/v1_6/index'
+          },
+          {
+            title: '快速上手',
+            path: '/doc/v1_6/quickstart',
+          },
+          {
+            title: '变更记录',
+            path: '/doc/v1_6/changelog',
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'WE-VUE 组件',
+    groups: [
+      {
+        groupName: '基础组件',
+        list: [
+          {
+            title: 'Badge',
+            path: '/doc/v1_6/badge'
+          },
+          {
+            title: 'Circle',
+            path: '/doc/v1_6/circle'
+          },
+          {
+            title: 'Icons',
+            path: '/doc/v1_6/icons'
+          },
+          {
+            title: 'Lazyload',
+            path: '/doc/v1_6/lazyload'
+          },
+          {
+            title: 'Progress',
+            path: '/doc/v1_6/progress'
+          },
+          {
+            title: 'Slider',
+            path: '/doc/v1_6/slider'
+          },
+          {
+            title: 'Spinner',
+            path: '/doc/v1_6/spinner'
+          },
+          {
+            title: 'Swipe',
+            path: '/doc/v1_6/swipe'
+          }
+        ]
+      },
+      {
+        groupName: '表单',
+        list: [
+          {
+            title: 'Button',
+            path: '/doc/v1_6/button'
+          },
+          {
+            title: 'CheckList',
+            path: '/doc/v1_6/check_list'
+          },
+          {
+            title: 'Input',
+            path: '/doc/v1_6/input'
+          },
+          {
+            title: 'NumberSpinner',
+            path: '/doc/v1_6/number_spinner'
+          },
+          {
+            title: 'Radio',
+            path: '/doc/v1_6/radio'
+          },
+          {
+            title: 'Switch',
+            path: '/doc/v1_6/switch'
+          },
+          {
+            title: 'Textarea',
+            path: '/doc/v1_6/textarea'
+          },
+          {
+            title: 'Preview',
+            path: '/doc/v1_6/preview'
+          }
+        ]
+      },
+      {
+        groupName: '操作反馈',
+        list: [
+          {
+            title: 'ActionSheet',
+            path: '/doc/v1_6/action_sheet'
+          },
+          {
+            title: 'DatetimePicker',
+            path: '/doc/v1_6/datetime_picker'
+          },
+          {
+            title: 'Dialog',
+            path: '/doc/v1_6/dialog'
+          },
+          {
+            title: 'InfiniteScroll',
+            path: '/doc/v1_6/infinite_scroll'
+          },
+          {
+            title: 'Picker',
+            path: '/doc/v1_6/picker'
+          },
+          {
+            title: 'Toast',
+            path: '/doc/v1_6/toast'
+          },
+          {
+            title: 'TopTips',
+            path: '/doc/v1_6/top_tips'
+          }
+        ]
+      },
+      {
+        groupName: '导航相关',
+        list: [
+          {
+            title: 'Header',
+            path: '/doc/v1_6/header'
+          },
+          {
+            title: 'Navbar',
+            path: '/doc/v1_6/navbar'
+          },
+          {
+            title: 'Tabbar',
+            path: '/doc/v1_6/tabbar'
+          }
+        ]
+      },
+      {
+        groupName: '搜索相关',
+        list: [
+          {
+            title: 'Search',
+            path: '/doc/v1_6/search'
+          }
+        ]
+      },
+      {
+        groupName: '布局',
+        list: [
+          {
+            title: 'Cell',
+            path: '/doc/v1_6/cell'
+          },
+          {
+            title: 'CellSwipe',
+            path: '/doc/v1_6/cell_swipe'
+          },
+          {
+            title: 'Flex',
+            path: '/doc/v1_6/flex'
+          },
+          {
+            title: 'Footer',
+            path: '/doc/v1_6/footer'
+          },
+          {
+            title: 'Grid',
+            path: '/doc/v1_6/grid'
+          },
+          {
+            title: 'Loadmore',
+            path: '/doc/v1_6/loadmore'
+          },
+          {
+            title: 'Panel',
+            path: '/doc/v1_6/panel'
+          },
+          {
+            title: 'Popup',
+            path: '/doc/v1_6/popup'
+          }
+        ]
+      },
+    ]
+  },
+  {
+    title: '其它',
+    groups: [
+      {
+        list: [
+          {
+            title: '问题解答',
+            path: '/doc/v1_6/troubleshooting'
+          },
+          {
+            title: '贡献指南',
+            path: '/doc/v1_6/contributing'
+          }
+        ]
+      }
+    ]
+
+  }
+]
