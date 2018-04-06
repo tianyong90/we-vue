5fe41a15790bacc039d89de02a79a0c3772e3d99
tianyong90 <412039588@qq.com>
2018-04-06 16:03:49 +0800

    [doc-site] remove animate.css

--- docs/app.vue
+++ docs/app.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <transition
-      name="custom-transition"
-      enter-active-class="animated fadeIn"
-      mode="out-in"
-    >
-      <router-view/>
-    </transition>
+    <router-view/>
   </div>
 </template>
 
--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
 import './assets/common.scss'
-import 'animate.css/animate.min.css'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
