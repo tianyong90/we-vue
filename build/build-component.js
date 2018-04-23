1c47da92344a3409690453b3c154f582a4fa3a41
tianyong90 <412039588@qq.com>
2018-04-24 01:00:28 +0800

    chore: remove ora package, we are using webpackbar now!

--- build/build-component.js
+++ build/build-component.js
@@ -3,20 +3,15 @@ require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.component.conf')
 
-const spinner = ora('building components...')
-spinner.start()
-
 rm(path.join(__dirname, '../lib/*'), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
       colors: true,
--- build/build-docs.js
+++ build/build-docs.js
@@ -3,20 +3,15 @@ require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.docsprod.conf')
 
-const spinner = ora('building docs...')
-spinner.start()
-
 rm(path.join(__dirname, '../docs-dist/*'), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
       colors: true,
