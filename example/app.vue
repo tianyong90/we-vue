9178ffbd47a9844b6b8ba38c4c8174f88cb03779
田勇 <412039588@qq.com>
2018-06-14 17:17:42 +0800

    docs: improve examples, add qrcode for each page.

--- example/app.vue
+++ example/app.vue
@@ -3,5 +3,36 @@
     <transition :name="$root.transitionName">
       <router-view/>
     </transition>
+    <div class="qrcode">
+      <img
+        :src="qrcodeUrl"
+        :class="{ 'qrcode-big': bigQrcode }"
+        @click="clickQrcode"
+      >
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      bigQrcode: false
+    }
+  },
+
+  computed: {
+    ...mapState({
+      qrcodeUrl: state => state.qrcodeUrl
+    })
+  },
+
+  methods: {
+    clickQrcode () {
+      this.bigQrcode = !this.bigQrcode
+    }
+  }
+}
+</script>
--- example/main.js
+++ example/main.js
@@ -4,14 +4,30 @@ import App from './app.vue'
 import './assets/style/demo.scss'
 import './assets/iconfont/iconfont.css'
 import router from './router'
+import QRCode from 'qrcode'
+import store from './store'
+import { mapState } from 'vuex'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
 Vue.use(WeVue)
 
-router.afterEach((to) => {
+router.afterEach((to, from) => {
   document.title = to.meta.title
+
+  const host = window.location.host
+  const protocol = window.location.protocol
+
+  const currentUrl = `${protocol}//${host}${to.fullPath}`
+
+  QRCode.toDataURL(currentUrl)
+    .then((url) => {
+      store.commit('UPDATE_QRCODE_URL', url)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 })
 
 /* eslint-disable no-new */
@@ -21,6 +37,8 @@ new Vue({
   render: h => h(App),
 
   router,
+
+  store,
 
   data () {
     return {
