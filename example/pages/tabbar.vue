1539ae16ac666bfeea49505a3a5aaad53c0d3ed6
tianyong90 <412039588@qq.com>
2018-07-11 11:40:44 +0800

    code style

--- example/pages/tabbar.vue
+++ example/pages/tabbar.vue
@@ -17,35 +17,48 @@
     <wv-tabbar>
       <wv-tabbar-item to="/" is-on>
         <span slot="icon" style="display: inline-block; position: relative;">
-          <img class="weui-tabbar__icon" :src="thumbSmall" slot="icon">
+          <img
+            class="weui-tabbar__icon"
+            src="../assets/images/icon_tabbar.png"
+            slot="icon"
+          >
           <wv-badge style="position: absolute;top: -2px;right: -13px;">8</wv-badge>
         </span>
         微信
       </wv-tabbar-item>
       <wv-tabbar-item to="/tabbar">
-        <img class="weui-tabbar__icon" :src="thumbSmall" slot="icon"> 通讯录
+        <img
+          class="weui-tabbar__icon"
+          src="../assets/images/icon_tabbar.png"
+          slot="icon"
+        > 通讯录
       </wv-tabbar-item>
       <wv-tabbar-item to="/">
         <span slot="icon" style="display: inline-block; position: relative;">
-          <img class="weui-tabbar__icon" :src="thumbSmall" slot="icon">
+          <img
+            class="weui-tabbar__icon"
+            src="../assets/images/icon_tabbar.png"
+            slot="icon"
+          >
           <wv-badge is-dot style="position: absolute;top: 0;right: -6px;">8</wv-badge>
         </span>
         发现
       </wv-tabbar-item>
       <wv-tabbar-item to="/profile">
-        <img class="weui-tabbar__icon" :src="thumbSmall" slot="icon"> 我
+        <img
+          class="weui-tabbar__icon"
+          src="../assets/images/icon_tabbar.png"
+          slot="icon"
+        > 我
       </wv-tabbar-item>
     </wv-tabbar>
   </div>
 </template>
 
 <script>
-import thumbSmall from '../assets/images/icon_tabbar.png'
-
 export default {
   data () {
     return {
-      thumbSmall
     }
   }
 }
