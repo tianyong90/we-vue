6bd8f4cf305543a6b5a362d971bad8eed71dc224
田勇 <412039588@qq.com>
2018-08-09 13:39:41 +0800

    build: update postcss-loader

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,6 +6,6 @@ module.exports = {
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {},
-    // "cssnano": {}
+    "cssnano": {}
   }
 }
