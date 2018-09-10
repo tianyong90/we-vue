68ad4ac9eb59e59cb15b00266082f46e2b304260
tianyong90 <412039588@qq.com>
2018-09-11 00:47:40 +0800

    scripts

--- build/build-lib.js
+++ build/build-lib.js
@@ -1,34 +1,24 @@
-'use strict'
-process.env.NODE_ENV = 'production'
+/**
+ * Build npm lib
+ */
+const shell = require('shelljs');
+const signale = require('signale');
+const { Signale } = signale;
+const tasks = [
+  'bootstrap',
+  'lint',
+  'build:entry',
+  'build:components',
+  'build:we-vue-css',
+  'build:style-entry',
+  'build:we-vue'
+];
 
-const rm = require('rimraf')
-const path = require('path')
-const chalk = require('chalk')
-const webpack = require('webpack')
-const webpackConfig = require('./webpack.prod.conf')
+tasks.forEach(task => {
+  signale.start(task);
 
-rm(path.join(__dirname, '../lib/index.js'), err => {
-  if (err) throw err
-})
-
-rm(path.join(__dirname, '../lib/style.min.css'), err => {
-  if (err) throw err
-})
-
-  webpack(webpackConfig, (err, stats) => {
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
-
-    if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
-      process.exit(1)
-    }
-
-  console.log(chalk.cyan('  Build complete.\n'))
-})
+  const interactive = new Signale({ interactive: true });
+  interactive.pending(task);
+  shell.exec(`npm run ${task} --silent`);
+  interactive.success(task);
+});
