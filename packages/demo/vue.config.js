b4c9245c95d3870044801406b7dadde0ab16691d
tianyong90 <412039588@qq.com>
2019-05-23 01:16:26 +0800

    chore: set devServer.watchOptions.ignore option
    this makes it possible to watch compiled we-vue library files (in node_modules/we-vue/dist) and hot replace the changed modules.

--- packages/demo/vue.config.js
+++ packages/demo/vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true
+    disableHostCheck: true,
+    /**
+     * IMPORTANT!!!
+     * 默认情况下会忽略 node_modules 目录，这会导致 node_modules/we-vue/dist 中的文件发生变化时不会更动热替换，不便调试
+     */
+    watchOptions: {
+      ignored: [string => string.includes('node_modules') && !string.includes('we-vue')]
+    }
   },
   css: {
     sourceMap: false
