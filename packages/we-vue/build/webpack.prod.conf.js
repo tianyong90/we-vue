d860b66c2de7640721ab00ff78b44097582182a8
tianyong90 <412039588@qq.com>
2019-07-23 17:41:29 +0800

    build: fix webpack config

--- packages/we-vue/build/webpack.prod.conf.js
+++ packages/we-vue/build/webpack.prod.conf.js
@@ -1,13 +1,8 @@
-const webpack = require('webpack')
 const path = require('path')
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
-const version = process.env.VERSION || require('../package.json').version
 
 module.exports = merge(baseWebpackConfig, {
   devtool: 'source-map',
@@ -17,7 +12,6 @@ module.exports = merge(baseWebpackConfig, {
   output: {
     path: path.join(__dirname, '../dist'),
     publicPath: '/dist/',
-    filename: 'we-vue.min.js',
     library: 'we-vue',
     libraryTarget: 'umd',
     libraryExport: 'default',
@@ -30,31 +24,6 @@ module.exports = merge(baseWebpackConfig, {
       commonjs2: 'vue',
       amd: 'vue',
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        path.join(__dirname, '../dist'),
-      ],
-      verbose: true,
-      dry: false,
-    }),
-    new webpack.BannerPlugin({
-      banner: `/*!
-* we-vue v${version}
-* Forged by Tian Yong
-* Released under the MIT License.
-*/     `,
-      raw: true,
-      entryOnly: true,
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'we-vue.min.css',
-    }),
-  ],
-  performance: {
-    hints: 'warning',
-    maxEntrypointSize: 500000,
   },
   optimization: {
     minimizer: [
