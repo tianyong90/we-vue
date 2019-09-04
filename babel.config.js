6aee1ba1dc0c4311a4a78740745a23ea566610ba
tianyong90 <412039588@qq.com>
2019-09-04 14:28:59 +0800

    babel config

--- babel.config.js
+++ babel.config.js
@@ -10,19 +10,12 @@ module.exports = function (api) {
             browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all'],
             node: 8,
           },
-          modules: false,
+          modules: 'commonjs',
         },
       ],
     ],
     plugins: [
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          corejs: false,
-          helpers: true,
-          regenerator: false,
-        },
-      ],
+      '@babel/plugin-transform-runtime',
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-transform-object-assign',
     ],
