f7c4256e3c9e1af17c46e05d615e03447ffdcecf
tianyong90 <412039588@qq.com>
2019-09-05 15:04:21 +0800

    fix(media-box): use v-html to bind media-box title
    close #122

--- src/components/media-box/index.vue
+++ src/components/media-box/index.vue
@@ -1,26 +1,26 @@
 <template>
   <div
+    v-if="type === 'appmsg'"
     class="weui-media-box"
     :class="'weui-media-box_' + type"
-    v-if="type === 'appmsg'"
     @click="onClick"
   >
     <div class="weui-media-box__hd" v-if="type !== 'text'">
       <img class="weui-media-box__thumb" :src="thumb" alt="">
     </div>
     <div class="weui-media-box__bd">
-      <h4 class="weui-media-box__title" v-text="title" />
+      <h4 class="weui-media-box__title" v-html="title" />
       <p class="weui-media-box__desc" v-text="description" />
     </div>
   </div>
 
   <div
+    v-else
     class="weui-media-box"
     :class="'weui-media-box_' + type"
-    v-else
     @click="onClick"
   >
-    <h4 class="weui-media-box__title" v-text="title" />
+    <h4 class="weui-media-box__title" v-html="title" />
     <p class="weui-media-box__desc" v-text="description" />
     <slot name="box_ft" />
   </div>
